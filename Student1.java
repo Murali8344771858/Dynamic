3961070dabd88496fe5a9e18675fba0bd142235b
Murali8344771858 <muraliprawin@gmail.com/mura>
2022-02-04 16:46:40 +0530

    Myfile

---
+++ Student1.java
@@ -0,0 +1,92 @@
+import java.io.*;
+class Student1
+{
+ public static void main(String args[])
+   {
+     int a=150;
+     int b=25;
+     int c=80;
+     if(a<b)
+     {
+          if(a<c)
+          System.out.println("Small="+a);
+          else
+          System.out.println("Small="+c);
+     }
+          else
+        {
+          if(b<c)
+          System.out.println("Small="+b);
+          else
+          System.out.println("Small="+c);
+        }
+    }
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
