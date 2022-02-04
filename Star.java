e2d16562fbb7ee5ff1c132c4668c83af354bd1d5
kiruthikavk <kiruthika280299@gmail.com>
2022-02-04 15:16:09 +0530

    myfile

---
+++ Star.java
@@ -0,0 +1,39 @@
+import java.io.*;
+class Star
+{
+ public static void main(String args[])
+    {
+      int n=3;
+      for(int i=0;i<=n;i++)
+        {
+          for(int k=n-i;k>=1;k--)
+            {
+              System.out.print(" ");
+            } 
+           for(int j=0;j<=i;j++)
+             {
+               
+              System.out.print(" * ");
+             }
+               System.out.println();
+          }
+   
+          int m=4;
+          for(int i=0;i<=m;i++)
+              {
+                for(int k=0;k<i;k++)
+                  {
+                    System.out.print("  ");
+                  }
+
+                    for(int j=3;j>i;j--)
+                       {
+                         System.out.print(" * ");
+                        }
+                         System.out.println();
+ }
+       }
+ }
+               
+             
+               
